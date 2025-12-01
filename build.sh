144aa9aced42bad8cec342a269489878a1740401
Jesse Meyer <jesse.r.meyer@me.com>
2025-12-01 11:45:01 -0500

    Update build script for Win32 support via Wine

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ if [[ -z "$BUILD_TYPE" ]]; then
   exit 1
 fi
 
-rm -f build/Norns_v3 build/Norns_Test_v3
+rm -f build/*
 
 OPT_FLAGS=""
 case "$BUILD_TYPE" in
@@ -43,20 +43,33 @@ BASE_BUILD_COMMAND=" $PRIMARY_SRC_FILE $WARN_FLAGS $BEHAVIOR_FLAGS $MATH_FLAGS $
 CLANG_BUILD_COMMAND="$COMPILER $BASE_BUILD_COMMAND"
 
 GCC_IGNORE_WARNINGS=" -Wno-unknown-pragmas -Wno-format"
-GCC_CHECK_COMMAND="g++ $BASE_BUILD_COMMAND $GCC_IGNORE_WARNINGS -fsyntax-only"
+GCC_CHECK_COMMAND="g++-14 $BASE_BUILD_COMMAND $GCC_IGNORE_WARNINGS -fsyntax-only"
 
-$CLANG_BUILD_COMMAND &
+$CLANG_BUILD_COMMAND -Xclang --compress-debug-sections=zstd &
 
-#Optionally enable GCC error checking.  GCC is more pedantic than Clang.
-$GCC_CHECK_COMMAND
+#Build Win32
+#WIN32_LIBS="-lkernel32 -lSynchronization -lUser32 -llibvcruntime"
+#wine /home/j/LLVM-21-1-6-Win32/bin/clang++.exe $PRIMARY_SRC_FILE $WARN_FLAGS $BEHAVIOR_FLAGS $MATH_FLAGS $OPT_FLAGS -o build/Norns_v3_win32 -flto -fuse-ld=lld -march=x86-64-v3 -Xmicrosoft-windows-sys-root /home/j/MSVC/msvc/ $WIN32_LIBS &
+
+#Apparently something like clang-cl -fuse-ld=lld -target x86_64-pc-windows-msvc -winsysroot folder/with/msvcandsdk file.c can work too (no need for wine).
+
+#Optionally enable GCC error checking.  GCC is more pedantic than Clang. 
+# Disabled by default since its support for concepts is quite limiting, particularly around dependency rules when evaluating types.
+#$GCC_CHECK_COMMAND
 
 PRIMARY_TEST_FILE="src/test/test.cpp"
 TEST_BUILD_COMMAND=" $PRIMARY_TEST_FILE $WARN_FLAGS $BEHAVIOR_FLAGS $MATH_FLAGS $OPT_FLAGS $LIBS -o build/Norns_Test_v3 -flto -fuse-ld=lld -march=x86-64-v3 -Isrc -DASAN -fsanitize=address,undefined"
-CLANG_BUILD_COMMAND="$COMPILER $TEST_BUILD_COMMAND"
+CLANG_BUILD_COMMAND="$COMPILER $TEST_BUILD_COMMAND -Xclang --compress-debug-sections=zstd"
 
-$CLANG_BUILD_COMMAND
+$CLANG_BUILD_COMMAND &
+
+#NOTE(Jesse): Wine crashes for some reason: err:virtual:virtual_setup_exception stack overflow
+#wine /home/j/LLVM-21-1-6-Win32/bin/clang++.exe $PRIMARY_TEST_FILE $WARN_FLAGS $BEHAVIOR_FLAGS $MATH_FLAGS $OPT_FLAGS -o build/Norns_Test_v3_win32 -flto -fuse-ld=lld -march=x86-64-v3 -Xmicrosoft-windows-sys-root /home/j/MSVC/msvc/ $WIN32_LIBS &
+
+wait $(jobs -p)
 
 ./build/Norns_Test_v3
+#wine build/Norns_Test_v3_win32
 
 if $ANALYZE; then
   BASE_BUILD_COMMAND+=" --analyze"
